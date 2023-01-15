6546b433f06319aac43f6aa0196d2b8eeb7ea747
Veronika <kazantceva_nika@bk.ru>
2023-01-15 23:11:53 +0300

    Задаем длину массива,задаем первый массив в строчном формате, задаем первый цикл, получаем первый результат массива

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Задаем длину массива: ");
+int size  = int.Parse(Console.ReadLine()!);
+
+string[] arr1 = new string[size];
+
+for (int i = 0; i < size; i++){
+	Console.Write("Введите элемент массива: ");
+    string result = Console.ReadLine();
+	arr1[i] = result;  
+}
